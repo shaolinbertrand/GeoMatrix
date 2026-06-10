f01e6214b9818e283f8c899b9569930697ae5c3a
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2026-06-10 15:55:08 -0400

    update
    retirando markdown do arquivo json e configurando o novo ip

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 //const API_IP = "localhost";
 //const API_IP = "192.168.18.121"
 //const API_IP = "192.168.0.111"
-const API_IP = "192.168.0.103"
+const API_IP = "172.22.66.31"
 const API_PORT = "3000";
 
 export const BASE_URL = `http://${API_IP}:${API_PORT}/api`;
